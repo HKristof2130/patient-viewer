f8bab9a8ee10ca1a619ca314a9485f69d3837fc3
HKristof2130 <78855300+HKristof2130@users.noreply.github.com>
2022-10-25 09:52:11 +0200

    more json files

--- src/app/services/patients.service.ts
+++ src/app/services/patients.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import data from 'src/assets/patients.json';
+import data1 from 'src/assets/patients.json';
+import data2 from 'src/assets/patients2.json';
+import data3 from 'src/assets/patients3.json';
 import { Gender } from '../enums/genders.enum';
 import { Patient } from '../interfaces/patient.interface';
 
@@ -11,18 +13,54 @@ export class PatientsService {
   patients : Patient[] = [] ;
 
   constructor() {
-   this.patients =  this.mapJsonToPatientArray() ;
+    this.patients =  this.mapJsonToPatientArray() ;
+    /*
+    this.maptToPatientArraySasiWay(data1,this.patients);
+    this.maptToPatientArraySasiWay(data2,this.patients);
+    this.maptToPatientArraySasiWay(data3,this.patients);
+    */
   }
 
   getPatientList() : Patient[]{
     return this.patients;
   }
 
+  private maptToPatientArraySasiWay(json: any, target: Patient[]){
+     
+    target.push(...json.list);
+  }
+
   private mapJsonToPatientArray() : Patient[]{
 
     let patinetsToMap : Patient[] = [] ;
     
-    data.forEach( element => {
+    data1.forEach( element => {
+
+      const tmpPatient : Patient = {
+        id : element.id,
+        name : element.name,
+        gender : this.getGender(element.gender),
+        dateOfBirth : new Date(element.dateOfBirth),
+      }
+
+      patinetsToMap.push(tmpPatient);
+ 
+    });
+
+    data2.list.forEach( element => {
+
+      const tmpPatient : Patient = {
+        id : element.id,
+        name : element.name,
+        gender : this.getGender(element.gender),
+        dateOfBirth : new Date(element.dateOfBirth),
+      }
+
+      patinetsToMap.push(tmpPatient);
+ 
+    });
+
+    data3.list.forEach( element => {
 
       const tmpPatient : Patient = {
         id : element.id,
@@ -42,7 +80,7 @@ export class PatientsService {
   private getGender(gender: string): Gender {
 
     switch (gender) {
-      
+
       case "male":
         return Gender.MALE;
 
